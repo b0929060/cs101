ccc901c5145c5a9ea5e03d6e79e073ec89df9703
b <mfps30521@yahoo.com.tw>
2022-04-14 18:28:14 +0800

    3

--- lab4/main.c
+++ lab4/main.c
@@ -5,40 +5,49 @@ int* find_num(int* p,int* n,int target) {
     for (int i=0;i<10;i++) {
         if (*n == target) {
            adr = i;
+           printf("&n[%d]->%p, n[%d]=%d; ",adr,&n,adr,*n);
            break;
         }
         n++;
     }
-    printf("&n[%d]->%p, n[%d]=%d; ",adr,&n,adr,*n);
-    printf("p->%p, *p=%d\n",p,*p);
+    return n;
 }
 
 int main() {
     int n[]={6,4,7,2,0,9,8,1,5,3};
-    int* p=n;
+    int* p;
     printf("No.1 --------------------\n");
-    find_num(p,n,9);
+    p=find_num(p,n,9);
+    printf("p->%p, *p=%d\n",p,*p);
     printf("No.2 --------------------\n");
     int* ap[10];
-    int indx[10];
+    int indx[10],m[10];
+    for (int i=0;i<10;i++) {
+        m[i]=n[i];
+    }
     //排序大小
 	for (int i = 0; i < 10; ++i) {          
 		for (int j = 0; j < 9 - i; ++j) { 
 			if (n[j] > n[j + 1]) {
 				int temp = n[j];
-				indx[j] = n[j + 1];
-				indx[j + 1] = temp;
+				n[j] = n[j + 1];
+				n[j + 1] = temp;
 			}
 		}
 	}
-// 	for (int i=0;i<10;i++) {
-//         printf("%d,",indx[i]);
-//     }
+	for (int i=0;i<10;i++) {
+	    for (int j=0;j<10;j++) {
+	        if (n[i]==m[j]) {
+	            indx[i]=j;
+	            break;
+	        }
+	    }
+	} 
     for (int i=0;i<10;i++) {
-        ap[i]=&n[i];
+        ap[i]=&(m[indx[i]]);
     }
     for (int i=0;i<10;i++) {
-        printf("&n[%i]->%p, n[%i]=%d; ",indx[i],&n[indx[i]],i,n[indx[i]]);
+        printf("&n[%i]->%p, n[%i]=%d; ",indx[i],&m[indx[i]],indx[i],m[indx[i]]);
         printf("ap[%i]->%p, *ap[%i]=%i\n",i,ap[i],i,*ap[i]);
     }
 
