bdb050b5643d14e28172bd11a46c3bf1a9a0f790
b <mfps30521@yahoo.com.tw>
2022-05-03 14:09:59 +0800

    lab6

---
+++ lab6/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int _sum(int x,int y) {
+    return x+y;
+}
+int _sub(int x,int y) {
+    return x-y;
+}
+int _mult(int x,int y) {
+    return x*y;
+}
+int _div(int x,int y) {
+    return x/y;
+}
+int _power(int x,int y) {
+    int ans=x;
+    for(int i=1;i<y;i++) {
+        ans*=x;
+    }
+    return ans;
+}
+
+int main() {
+    int x,y,choice;
+    int(*ope[5])(int,int);
+    ope[0]=_sum;
+    ope[1]=_sub;
+    ope[2]=_mult;
+    ope[3]=_div;
+    ope[4]=_power;
+    
+    printf("Enter two integer: ");
+    scanf("%d%d",&x,&y);
+    
+    printf("Enter 0:+, 1:-, 2:*,3:/, 4:^ ");
+    scanf("%d",&choice);
+    
+    printf("%d",ope[choice](x,y));
+    return 0;
+}
